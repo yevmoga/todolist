9ae2d98f9b96e3d359d9a54057d29be3077073f7
Yevhen Moha <yevmoga@gmail.com>
2019-04-12 16:22:20 +0300

    isntall gin

--- src/main.go
+++ src/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	fmt.Println("Hello world!")
+
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World",
+		})
+	})
+
+	r.Run()
 }
